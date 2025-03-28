fb3eb5029f140f5728dacba9a07535a365c6f081
Alexander <61624959+russianroadman@users.noreply.github.com>
2025-03-28 16:55:59 +0800

    Add files via upload

---
+++ chatix-core/src/main/java/com/russianroadman/chatix_core/ChatixCoreApplication.java
@@ -0,0 +1,13 @@
+package com.russianroadman.chatix_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatixCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatixCoreApplication.class, args);
+	}
+
+}
---
+++ chatix-core/src/test/java/com/russianroadman/chatix_core/ChatixCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.russianroadman.chatix_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatixCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
